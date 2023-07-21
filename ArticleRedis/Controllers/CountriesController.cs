0e3ffe51bbcf01013bdfdf9c9947e926d7cc223f
Abnoan Muniz <abnoanmuniz@gmail.com>
2023-07-21 13:16:55 -0300

    Article

---
+++ ArticleRedis/Controllers/CountriesController.cs
@@ -0,0 +1,49 @@
+﻿using ArticleRedis.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace ArticleRedis.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private const string CountriesKey = "Countries";
+        private const string RestCountriesUrl = "https://restcountries.eu/rest/v2/all";
+
+        public CountriesController(IDistributedCache distributedCache, IHttpClientFactory httpClientFactory)
+        {
+            _distributedCache = distributedCache;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            var countriesObject = await _distributedCache.GetStringAsync(CountriesKey);
+
+            if (!string.IsNullOrWhiteSpace(countriesObject))
+            {
+                return Ok(countriesObject);
+            }
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync(RestCountriesUrl);
+            var responseData = await response.Content.ReadAsStringAsync();
+
+            var countries = JsonConvert.DeserializeObject<List<Country>>(responseData);
+
+            var memoryCacheEntryOptions = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                SlidingExpiration = TimeSpan.FromSeconds(1200)
+            };
+
+            await _distributedCache.SetStringAsync(CountriesKey, responseData, memoryCacheEntryOptions);
+
+            return Ok(countries);
+        }
+    }
+}
---
+++ ArticleRedis/Models/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace ArticleRedis.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public string Region { get; set; }
+        public string Demonym { get; set; }
+    }
+}
